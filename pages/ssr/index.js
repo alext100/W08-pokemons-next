8002cec2bc647fe1adcb3282d60209ab45552e61
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-16 22:48:22 +0100

    Page Pokemons SSR created

--- pages/ssr/index.js
+++ pages/ssr/index.js
@@ -1,4 +1,32 @@
-const PokemonSSR = () => {
-  return <h1>PokemonSSR</h1>;
+const PokemonSSR = ({ pokemons }) => {
+  return (
+    <>
+      <h1>Pokemons SSR</h1>
+      <ul>
+        {pokemons &&
+          pokemons.map((pokemon) => (
+            <li key={pokemon.id}>
+              {pokemon.name}
+              <img
+                src={pokemon.sprites.other.dream_world.front_default}
+                alt={`Pokemon ${pokemon.name} image`}
+                style={{ width: "20%" }}
+              />
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 };
 export default PokemonSSR;
+
+export const getServerSideProps = async () => {
+  const response = await fetch(
+    "https://pokemon-api-aleksandr.herokuapp.com/pokemon"
+  );
+  const pokemons = await response.json();
+  console.log("pokemons: ", pokemons);
+  return {
+    props: { pokemons },
+  };
+};
