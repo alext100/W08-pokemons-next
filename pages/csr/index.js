31172d097a471212b5899cc9e5816a5f633bf3b4
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-16 22:27:46 +0100

    Page Pokemon CSR created

--- pages/csr/index.js
+++ pages/csr/index.js
@@ -1,4 +1,39 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const PokemonCSR = () => {
-  return <h1>PokemonCSR</h1>;
+  const [pokemonsList, setPokemonsList] = useState([]);
+  const [pokemons, setPokemon] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10`);
+      const { results } = await res.json();
+      setPokemonsList(results);
+      const urlList = pokemonsList.map((pokemon) => pokemon.url);
+      const result = await Promise.all(urlList.map((url) => fetch(url)));
+      const pokemons = await Promise.all(result.map((res) => res.json()));
+      setPokemon(pokemons);
+    })();
+  }, [pokemons]);
+
+  return (
+    <>
+      <h1>PokemonCSR</h1>
+      <ul>
+        {pokemons &&
+          pokemons.map((pokemon) => (
+            <li key={pokemon.id}>
+              {pokemon.name}
+              <img
+                src={pokemon.sprites.other.dream_world.front_default}
+                alt={`Pokemon ${pokemon.name} image`}
+                style={{ width: "20%" }}
+              />
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 };
 export default PokemonCSR;
